774e6915b067753999098cd7960dbcd6b684308e
James Hines <jameshines10@gmail.com>
2019-01-02 19:06:27 -0600

    Fixes compilation error

--- src/main/java/com/jhinesconsulting/FavoriteColor.java
+++ src/main/java/com/jhinesconsulting/FavoriteColor.java
@@ -1,7 +1,45 @@
 package com.jhinesconsulting;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Properties;
+
 public class FavoriteColor {
     public static void main(String[] args) {
+        Properties streamsConfiguration = new Properties();
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "favorite-color-application");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "54.183.72.166:9092");
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        StreamsBuilder builder = new StreamsBuilder();
+
+        final KStream<String, String> usersAndColorsPlainTextInput = builder.stream("users-and-colors-plaintext");
+
+        final Serde<String> stringSerde = Serdes.String();
+
+        final KStream<String, String> userNameAsKeyColorAsValue = usersAndColorsPlainTextInput
+                // 1. Generate a key based on the username
+                .selectKey((key, value) -> value.split(",")[0])
+                // 2. Map values so that they contain only colors
+                .mapValues(color -> color.split(",")[1]);
+
+        userNameAsKeyColorAsValue.to("username-key-color-value", Produced.with(Serdes.String(), Serdes.String()));
+
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
+        streams.cleanUp();
+        streams.start();
+
+        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
